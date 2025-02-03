5fc2438db2fbca2b4dd80aaf2736522888accf70
AFreborn22 <aafauzan52@gmail.com>
2025-02-03 18:20:29 +0700

    add register for testing end point login

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -1,6 +1,33 @@
 const bcrypt = require("bcryptjs");
-const jwt = require("../Middleware/authMiddleware");
+const jwt = require('jsonwebtoken');
 const { Admin } = require("../models");  
+
+exports.Register = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    if (!username || !password) {
+      console.log('Field kosong ditemukan');
+      return res.status(400).send({ message: 'Semua field harus diisi.' });
+    }
+    
+    const existingUser = await Admin.findOne({ where: { username } });
+    if (existingUser) {
+      console.log(`username sudah terdaftar: ${username}`);
+      return res.status(400).send({ message: 'username sudah terdaftar. Silakan gunakan username lain.' });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newAdmin = await Admin.create({
+      username,
+      password: hashedPassword,
+    });
+
+    res.status(201).send({ message: 'Admin berhasil terdaftar', newAdmin });
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
 
 exports.Login = async (req, res) => {
   const { username, password } = req.body;
@@ -26,7 +53,7 @@ exports.Login = async (req, res) => {
     //JWT token
     const token = jwt.sign({ id: admin.id }, process.env.JWT_SECRET, { expiresIn: "1h" });
 
-    return res.json({ token, username: admin.username });
+    return res.json({ username: admin.username, token });
   } catch (err) {
     return res.status(500).json(
       { error: "Terjadi kesalahan pada server", details: err.message });
--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { Login } = require("../controllers/authController");
+const { Register, Login } = require("../controllers/authController");
 
 const router = express.Router();
 
+router.post("/register", Register);
 router.post("/login", Login);
 
 module.exports = router;
